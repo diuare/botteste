9c7f0b225090c97f2ddbe7406d8263f7a60be1c4
Diuare <diuare.priv@gmail.com>
2021-04-05 21:50:27 -0300

    First commit

---
+++ config.js
@@ -0,0 +1,54 @@
+const ownerNumber = ["5547988652863@s.whatsapp.net"] 
+// mude somente o numero e não tire o @s.whatsapp.net
+
+const OriginalOwner = '5547988652863' 
+// mude para seu numero
+
+const cr = 'N e k o B O T\n' 
+// texto do simbolo de verificado
+
+const prefix = '!' 
+// prefixo
+
+const msgerr = 'Deu erro, tente novamente :/'
+// mensagem de erro
+
+const apiTobz = 'key'
+// pra pegar a key do Tobz va até o numero 
+//wa.me/447418349954 se registre no bot
+// e depois escreva #generateapikey que o 
+//bot do tobz vai gerar sua key
+
+let mess = {
+    wait: '⌛ Aguarde um pouco... ⌛',
+    success: '✔️ Sucesso! ✔️',
+    error: {
+        stick: '❌ Falha, ocorreu um erro ao converter a imagem em um adesivo ❌',
+        Iv: '❌ Link inválido ❌'
+    },
+    only: {
+        group: '❌ Este comando só pode ser usado em grupos! ❌',
+        ownerG: '❌ Este comando só pode ser usado pelo grupo proprietário! ❌',
+        ownerB: '❌ Este comando só pode ser usado pelo número proprietário! ❌',
+        admin: '❌ SILÊNCIO MEMBRO COMUM VC N TEM MORAL PRA USAR ESSE COMANDO ❌',
+        Badmin: '❌ Este comando só pode ser usado quando o bot se torna administrador! ❌'
+    }
+}
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:Meu criador^~^\n' 
++ 'ORG:Ian;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=557187645787:+55 71 8764-5787\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+exports.msgerr = msgerr
+exports.mess = mess
+exports.apiTobz = apiTobz
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
